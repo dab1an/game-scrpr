fb516a2e95f85ad99fa11a1e1972f825438fbd9e
dab1an <dabianextras@gmail.com>
2023-08-15 21:45:21 -0400

    Tailwind setup, routing setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app flex flex-col items-center w-full bg-white">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"></Route>
+          <Route path="/market" element={<ExplorePage />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
