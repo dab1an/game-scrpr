ff18eecdc5b770f8c278be831c838b06f0ec81fe
dab1an <dabianextras@gmail.com>
2023-08-17 15:03:16 -0400

    Finished product containers, added props

--- src/App.js
+++ src/App.js
@@ -7,7 +7,21 @@ function App() {
     <div className="app flex flex-col items-center w-full bg-white">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Product />}></Route>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Product
+                  title="Nintendo switch pro controller for blah blah blah blah blah blah blah blah blah blah blahl Blah"
+                  productImage="https://picsum.photos/125/125"
+                  price="$30"
+                  salePrice="$20"
+                  percentOff="%10"
+                  link="https://www.amazon.com/Charger-Nintendo-Adapter-Compatible-Support/dp/B07MVG7H8V/ref=sr_1_1?qid=1692295792&refinements=p_n_deal_type%3A23566064011&rnid=23566063011&s=videogames&sr=1-1"
+                />
+              </div>
+            }
+          ></Route>
           <Route path="/test"></Route>
         </Routes>
       </BrowserRouter>
--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 
-const Product = () => {
+const Product = (props) => {
   return (
-    <div className="flex items-center bg-white border-[3px] border-solid border-gray-300 rounded-md p-3 shadow-md w-1/4">
-      <img src="https://picsum.photos/125/125" alt="" className="" />
+    <div className="flex items-center bg-white border-[3px] border-solid border-gray-300 rounded-md p-3 shadow-md w-[450px]">
+      <img src={props.productImage} alt="" className="rounded-sm" />
 
       <div className="flex flex-col p-2">
-        <h1 className="text-xl break-normal	text-ellipsis overflow-hidden whitespace-nowrap w-[275px]">
-          Wireless Switch Controller for Nintendo Switch/Lite/OLED Controller,
-          Switch Controller with a Mouse Touch Feeling on Back Buttons, Extra
-          Switch Pro Controller with Wake-up,Programmable, Turbo Function
-        </h1>
-        <p>Desc</p>
-        <div className="flex gap-2 text-lg">
-          <p className="line-through">$20</p>
-          <p>$30</p>
+        <h1 className="text-xl multiline">{props.title}</h1>
+        <div className="w-1/6 flex justify-center bg-green-600 text-white rounded-md">
+          <p>{props.percentOff}</p>
         </div>
-
-        <a href=""> Link</a>
+        <div className="flex gap-2 text-lg pt-1">
+          <p className="line-through">{props.price}</p>
+          <p>{props.salePrice}</p>
+        </div>
+        <a href={props.link} target="_blank" className="text-blue-500">
+          Link
+        </a>
       </div>
     </div>
   );
