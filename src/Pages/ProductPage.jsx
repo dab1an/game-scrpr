3d45ee49e211d7e3c3177211e25f2b71de301a5b
dab1an <dabianextras@gmail.com>
2023-08-23 23:07:51 -0400

    Fixed pagination by adding delay and changing loops

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,48 +1,44 @@
-import { useEffect, useState } from "react";
-import React from "react";
-import Product from "../Components/Product";
+import axios from "axios";
+import React, { useState } from "react";
 
-const fetchData = async function () {
-  const response = await fetch("http://localhost:8001", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+const ZipBox = () => {
+  const [zip, setZip] = useState("");
 
-  const data = await response.json();
-  return data;
-};
-
-const ProductPage = () => {
-  const [products, setProducts] = useState();
-
-  useEffect(function () {
-    async function fetch() {
-      const data = await fetchData();
-      setProducts(data);
-      console.log(data);
-    }
-    fetch();
-  }, []);
-
-  if (!products || !products.length) {
-    return <p>loading</p>;
+  async function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .get(`http://localhost:8001/zip?paramZip=${zip}`)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    console.log(zip);
   }
 
   return (
-    <div>
-      {products.map((product) => (
-        <Product
-          title={product.title}
-          price={product.price}
-          salePrice={product.salePrice}
-          discount={product.discount}
-          productImage={product.productImage}
-          key={product.title}
-        />
-      ))}
+    <div className=" p-2 flex justify-center items-center h-24">
+      <form onSubmit={handleSubmit} className="flex w-full">
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Enter Zip Code"
+            className="pl-7 w-2/3 h-[60px] text-2xl rounded-tl-md rounded-bl-md border border-gray-500 border-r-0 outline-none"
+            onChange={(e) => {
+              setZip(e.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            className=" bg-black text-white p-1 rounded-tr-md rounded-br-md w-1/3 text-2xl font-bold border-gray-500"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
 
-export default ProductPage;
+export default ZipBox;
