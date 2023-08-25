53222c0472360b4cf9dc459b583b7b454dab5622
dab1an <dabianextras@gmail.com>
2023-08-25 11:30:08 -0400

    Added navigation for filling out user zip information

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
 import React from "react";
-import ZipBox from "../Components/ZipBox";
+import Product from "../Components/Product";
+
+const fetchData = async function () {
+  const response = await fetch("http://localhost:8001", {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  return data;
+};
 
 const Home = () => {
-  return <ZipBox />;
+  const [products, setProducts] = useState();
+
+  useEffect(function () {
+    async function fetch() {
+      const data = await fetchData();
+      setProducts(data);
+      console.log(products);
+    }
+    fetch();
+  }, []);
+
+  if (!products || !products.length) {
+    return <p>loading</p>;
+  }
+
+  return (
+    <div>
+      {products.map(function (product) {
+        <Product
+          title={product.title}
+          price={product.price}
+          salePrice={product.salePrice}
+          percentOff={product.discount}
+          productImage={product.productImage}
+        />;
+      })}
+    </div>
+  );
 };
 
 export default Home;
--- src/scraper.js
+++ src/scraper.js
@@ -1,10 +1,11 @@
 import puppeteer from "puppeteer";
 
-export const scraper = async () => {
+export const scraper = async (userZip) => {
   const browser = await puppeteer.launch({
     userDataDir: "./tmp",
     headless: false,
     defaultViewport: false,
+    args: ["--incognito"],
   });
   const page = await browser.newPage();
 
@@ -12,95 +13,115 @@ export const scraper = async () => {
     "https://www.amazon.com/s?bbn=468642&rh=n%3A468642%2Cp_n_deal_type%3A23566064011&dc&qid=1691454769&rnid=23566063011"
   );
 
-  let btnDisabled = false;
-  let itemList = [];
+  const popOverBox = await page.$("#nav-global-location-popover-link");
+  await popOverBox.click();
+  await page.waitForTimeout(2000);
+  await page.type("#GLUXZipUpdateInput", "33183"), { delay: 800 };
 
-  while (!btnDisabled) {
-    //wait for all pending requests to resolve
+  const applyZip = await page.$("#GLUXZipUpdate > span > input");
+  await page.waitForTimeout(800);
+  await applyZip.click();
 
-    await page.waitForSelector("div.s-result-list");
-    const products = await page.$$("div.s-result-item");
-    console.log("\n\nfound " + products.length + " products\n\n");
+  await page.waitForTimeout(800);
+  const zipDone = await page.$(
+    "#a-popover-2 > div > div.a-popover-footer > span > span"
+  );
 
-    for (const product of products) {
-      let item = new Object();
+  // await zipDone.click();
+  //   let btnDisabled = false;
+  //   let itemList = [];
 
-      try {
-        item.title = await page.evaluate(
-          (el) =>
-            el.querySelector(".s-title-instructions-style > h2 > a > span")
-              .textContent,
-          product
-        );
-        console.log("Title", item.title);
+  //   while (!btnDisabled) {
+  //     //wait for all pending requests to resolve
 
-        item.salePrice = await page.evaluate(
-          (el) => el.querySelector(".a-price > .a-offscreen").textContent,
-          product
-        );
+  //     await page.waitForSelector("div.s-result-list");
+  //     const products = await page.$$("div.s-result-item");
+  //     console.log("\n\nfound " + products.length + " products\n\n");
 
-        item.price = await page.evaluate(
-          (el) =>
-            el.querySelector(
-              "a > div > span.a-price.a-text-price > span.a-offscreen"
-            ).textContent,
-          product
-        );
+  //     for (const product of products) {
+  //       let item = new Object();
 
-        item.discount = Math.round(
-          ((parseFloat(item.price.substring(1)) -
-            parseFloat(item.salePrice.substring(1))) /
-            parseFloat(item.price.substring(1))) *
-            100
-        );
+  //       try {
+  //         item.title = await page.evaluate(
+  //           (el) =>
+  //             el.querySelector(".s-title-instructions-style > h2 > a > span")
+  //               .textContent,
+  //           product
+  //         );
+  //         console.log("Title", item.title);
 
-        item.productImage = await page.evaluate(
-          (el) => el.querySelector(".s-image").getAttribute("src"),
-          product
-        );
+  //         item.salePrice = await page.evaluate(
+  //           (el) => el.querySelector(".a-price > .a-offscreen").textContent,
+  //           product
+  //         );
 
-        itemList.push(item);
-      } catch (error) {
-        //console.log(product);
-        //console.log(error);
-      }
-    }
+  //         item.price = await page.evaluate(
+  //           (el) =>
+  //             el.querySelector(
+  //               "a > div > span.a-price.a-text-price > span.a-offscreen"
+  //             ).textContent,
+  //           product
+  //         );
 
-    await page.waitForSelector(
-      "a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator",
-      { visible: true }
-    ); // Waits for the button to be visible on the page
+  //         item.discount = Math.round(
+  //           ((parseFloat(item.price.substring(1)) -
+  //             parseFloat(item.salePrice.substring(1))) /
+  //             parseFloat(item.price.substring(1))) *
+  //             100
+  //         );
 
-    const nextButton = await page.$(
-      "a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator"
-    );
+  //         item.productImage = await page.evaluate(
+  //           (el) => el.querySelector(".s-image").getAttribute("src"),
+  //           product
+  //         );
 
-    if (nextButton) {
-      const isDisabled = await nextButton.$(
-        "span.s-pagination-item.s-pagination-next.s-pagination-disabled"
-      );
+  //         itemList.push(item);
+  //       } catch (error) {
+  //         //console.log(product);
+  //         //console.log(error);
+  //       }
+  //     }
 
-      if (!isDisabled) {
-        await nextButton.click();
-      }
-    }
+  //     await page.waitForSelector(
+  //       "a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator",
+  //       { visible: true }
+  //     ); // Waits for the button to be visible on the page
 
-    await page.waitForTimeout(1500);
-  }
+  //     const nextButton = await page.$(
+  //       "a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator"
+  //     );
 
-  itemList = itemList.filter((item) => item.title);
-  itemList = itemList.filter((item) => item.price);
-  itemList = itemList.filter((item) => item.discount);
-  itemList = itemList.filter((item) => item.productImage);
+  //     if (nextButton) {
+  //       const isDisabled = await nextButton.$(
+  //         "span.s-pagination-item.s-pagination-next.s-pagination-disabled"
+  //       );
 
-  for (let i = 0; i < itemList.length; i++) {
-    console.log(i + " " + itemList[i].title);
-    console.log();
-  }
+  //       if (!isDisabled) {
+  //         await nextButton.click();
+  //       }
 
-  await browser.close();
+  //       if (isDisabled) {
+  //         btnDisabled = true;
+  //       }
+  //     }
 
-  return itemList;
+  //     await page.waitForTimeout(1500);
+  //   }
+
+  //   itemList = itemList.filter((item) => item.title);
+  //   itemList = itemList.filter((item) => item.price);
+  //   itemList = itemList.filter((item) => item.discount);
+  //   itemList = itemList.filter((item) => item.productImage);
+
+  //   for (let i = 0; i < itemList.length; i++) {
+  //     console.log(i + " " + itemList[i].title);
+  //     console.log();
+  //   }
+
+  //   await browser.close();
+
+  //   return itemList;
+  // };
 };
 
 scraper();
